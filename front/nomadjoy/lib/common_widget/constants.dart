669583f539eb5dc2e325fd1c6f3ce841bcc9b38d
CarTig <carolle.t.n@gmail.com>
2024-07-18 14:48:27 +0200

    composant card font color

---
+++ front/nomadjoy/lib/common_widget/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color purpuleColor = Color(0xFF806491);
+const Color pinkColor = Color(0xFFB9848C);
+const Color blueColor = Color(0xFF2F70AF);
--- front/nomadjoy/lib/main.dart
+++ front/nomadjoy/lib/main.dart
@@ -1,87 +1,37 @@
 import 'package:flutter/material.dart';
+import 'common_widget/general_card.dart';
+import 'common_widget/constants.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          leading: const Icon(Icons.people_outline),
+          leading: const Icon(Icons.person),
           title: Image.asset('images/nomadjoy_logo.png', height: 30,),
           
         ),
 
-            body: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Text(
-                  'Titre de la Section',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: 5, // Nombre de cartes à afficher
-                    itemBuilder: (context, index) {
-                      return Card(
-                        elevation: 5,
-                        margin: const EdgeInsets.symmetric(vertical: 10),
-                        child: Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Image.asset(
-                                'images/film_palmiers.jpg',
-                                height: 100,
-                                width: double.infinity,
-                                fit: BoxFit.cover,
-                              ),
-                              const SizedBox(height: 10),
-                              Text(
-                                'Titre ${index + 1}',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                'Sous-titre ${index + 1}',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: <Widget>[
-                                  Text(
-                                    'Note: ${index + 1}/5',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  IconButton(
-                                    icon: Icon(Icons.add),
-                                    onPressed: () {
-                                      // Logique pour ajouter un nouvel élément
-                                    },
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ],
+        body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Top 5 Livres',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
+            GeneralCard(
+              imagePath: 'images/film_palmiers.jpg',
+              title: 'Titre 1',
+              subtitle: 'Sous-titre 1',
+              rating: 1,
+            ),
+          ],
+        ),
       ),
     bottomNavigationBar: BottomNavigationBar(
       items: const [
@@ -89,8 +39,8 @@ void main() {
         BottomNavigationBarItem(label: 'Noter', icon: Icon(Icons.add))
       ],
       backgroundColor: Colors.white,
-      selectedItemColor: Color(0xFF806491),
-      unselectedItemColor: Color(0xFFB9848C),
+      selectedItemColor: purpuleColor,
+      unselectedItemColor: pinkColor,
       showSelectedLabels: false,
       showUnselectedLabels: false,
       iconSize: 40,
